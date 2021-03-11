6ed9b00a937471265ca8d60501624179b95b54f2
430lyj <75655613+430lyj@users.noreply.github.com>
2021-03-12 02:10:29 +0900

    programmers.co.kr Test Score in python
    프로그래머스 모의고사 파이썬 풀이입니다.

--- testScore.py
+++ testScore.py
@@ -20,4 +20,4 @@ def solution(answers):
         if correct[i] == MAX:
             answer.append[i]
     return answer
-  #출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+  # 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
