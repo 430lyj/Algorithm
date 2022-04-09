66702fa41f894544a916310dc16184f9704984d5
430lyj <430lyj@gmail.com>
2022-04-09 13:09:50 +0900

    4/9

--- python/baekjoon_2110.py
+++ python/baekjoon_2110.py
@@ -1,22 +1,28 @@
 n, m = tuple(map(int, input().split()))
 arr = [int(input()) for _ in range(n)]
-
 arr.sort()
+
 left, right = 1, arr[-1] - arr[0] # 가장 인접한 두 물건의 거리 최소값, 가능한 최대값
 result = 0
+
 while left <= right:    
     mid = (left + right) // 2
-    current = arr[0]
     cnt = 1
+    current = arr[0]
+    possible = False
 
     for i in range(1, len(arr)):
         if arr[i] >= current + mid:
+            if arr[i] == current + mid:
+                possible = True
             cnt += 1
             current = arr[i]
+            
     
     if cnt >= m:
         left = mid+1
-        result = mid 
+        if possible :
+            result = mid 
     else:
         right = mid - 1
 
