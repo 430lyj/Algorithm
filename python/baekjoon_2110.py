2acc18bb06b8013a49ecefbbad2854348f9a7f50
430lyj <430lyj@gmail.com>
2022-04-09 13:42:52 +0900

    4/9

--- python/baekjoon_2110.py
+++ python/baekjoon_2110.py
@@ -1,29 +1,27 @@
-n, m = tuple(map(int, input().split()))
+import sys
+input = sys.stdin.readline
+n, m = tuple(map(int, input().rstrip().split()))
 arr = [int(input()) for _ in range(n)]
 arr.sort()
 
 left, right = 1, arr[-1] - arr[0] # 가장 인접한 두 물건의 거리 최소값, 가능한 최대값
-result = 0
+result = []
 
 while left <= right:    
     mid = (left + right) // 2
     cnt = 1
     current = arr[0]
-    possible = False
 
-    for i in range(1, len(arr)):
+    for i in range(1, n):
         if arr[i] >= current + mid:
-            if arr[i] == current + mid:
-                possible = True
             cnt += 1
             current = arr[i]
             
     
     if cnt >= m:
         left = mid+1
-        if possible :
-            result = mid 
+        result.append(mid)
     else:
         right = mid - 1
 
-print(result)
+print(max(result))
